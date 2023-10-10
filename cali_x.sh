836c6b90636175c6e50e5920ee0818f35fad8439
Kyle McCreery <mccreery.kyle@gmail.com>
2023-10-10 16:48:23 -0400

    Autocommit from 2023-10-10 16:48:23
    Klipper on commit: 5edc7fe
    
    Moonraker on commit: ee62d07
    
    Mainsail version: v2.7.1

---
+++ cali_x.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+
+/usr/bin/python /home/pi/klipper/scripts/calibrate_shaper.py $(ls -Art /tmp/resonances_x_*.csv | tail -n 1) -o /tmp/shaper_calibrate_x.png | grep -i recommended
+
---
+++ cali_y.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+
+/usr/bin/python /home/pi/klipper/scripts/calibrate_shaper.py $(ls -Art /tmp/resonances_y_*.csv | tail -n 1) -o /tmp/shaper_calibrate_y.png | grep -i recommended
+
