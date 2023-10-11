8101118c190b5c492f39394a5f232adc5bdc829e
Kyle McCreery <mccreery.kyle@gmail.com>
2023-10-11 15:14:46 -0400

    Autocommit from 2023-10-11 15:14:46
    Klipper on commit: 5edc7fe
    
    Moonraker on commit: ee62d07
    
    Mainsail version: v2.8.0

--- cali_x.sh
+++ cali_x.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
-/usr/bin/python /home/pi/klipper/scripts/calibrate_shaper.py $(ls -Art /tmp/resonances_x_*.csv | tail -n 1) -o /tmp/shaper_calibrate_x.png | grep -i recommended
-
+/usr/bin/python /home/pi/klipper/scripts/calibrate_shaper.py $(ls -Art /tmp/resonances_x_*.csv | tail -n 1) -c /tmp/shaper_calibrate_x.csv
+#/usr/bin/python /home/pi/klipper/scripts/calibrate_shaper.py $(ls -Art /tmp/resonances_x_*.csv | tail -n 1) -o /tmp/shaper_calibrate_x.png | grep -i recommended
--- cali_y.sh
+++ cali_y.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/sh
-
-/usr/bin/python /home/pi/klipper/scripts/calibrate_shaper.py $(ls -Art /tmp/resonances_y_*.csv | tail -n 1) -o /tmp/shaper_calibrate_y.png | grep -i recommended
-
+/usr/bin/python /home/pi/klipper/scripts/calibrate_shaper.py $(ls -Art /tmp/resonances_y_*.csv | tail -n 1) -c /tmp/shaper_calibrate_y.csv
+# /usr/bin/python /home/pi/klipper/scripts/calibrate_shaper.py $(ls -Art /tmp/resonances_y_*.csv | tail -n 1) -o /tmp/shaper_calibrate_y.png | grep -i recommended
